f53ab51dc1b724740b94232810b0d5debc83629b
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-07-19 18:05:55 -0700

    Added integration tests

---
+++ tests/factor_prime_test.rs
@@ -0,0 +1,13 @@
+use archimedes::factoring::prime_factors::prime_factors;
+use archimedes::is_prime::is_prime;
+
+#[test]
+fn all_prime_factors_are_prime() {
+    for num in 1..100 {
+        let prime_factors = prime_factors(num);
+
+        for factor in prime_factors {
+            assert!(is_prime(factor));
+        }
+    }
+}
