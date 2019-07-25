146cfb971505e1550a5dd2a78626272f40e9c9b7
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-07-24 18:52:02 -0700

    Implemented most math operations on Rational

--- src/rational.rs
+++ src/rational.rs
@@ -1,3 +1,7 @@
+use std::cmp;
+use std::ops;
+
+#[derive(Debug)]
 pub struct Rational {
     top: i64,
     bottom: i64,
@@ -13,5 +17,81 @@ impl Rational {
 impl From<i64> for Rational {
     fn from(value: i64) -> Rational {
         Rational::new(value, 1)
+    }
+}
+
+impl ops::Add<Rational> for Rational {
+    type Output = Rational;
+    fn add(self, rhs: Rational) -> Rational {
+        Rational {
+            top: (self.top * rhs.bottom) + (self.bottom * rhs.top),
+            bottom: self.bottom * rhs.bottom,
+        }
+    }
+}
+
+impl ops::Sub<Rational> for Rational {
+    type Output = Rational;
+    fn sub(self, rhs: Rational) -> Rational {
+        Rational {
+            top: (self.top * rhs.bottom) - (self.bottom * rhs.top),
+            bottom: self.bottom * rhs.bottom,
+        }
+    }
+}
+
+impl ops::Neg for Rational {
+    type Output = Rational;
+    fn neg(self) -> Rational {
+        if self.bottom < 0 {
+            Rational {
+                bottom: -self.bottom,
+                ..self
+            }
+        } else {
+            Rational {
+                top: -self.top,
+                ..self
+            }
+        }
+    }
+}
+
+impl ops::Mul<Rational> for Rational {
+    type Output = Rational;
+    fn mul(self, rhs: Rational) -> Rational {
+        Rational {
+            top: self.top * rhs.top,
+            bottom: self.bottom * rhs.bottom,
+        }
+    }
+}
+
+impl ops::Div<Rational> for Rational {
+    type Output = Rational;
+    fn div(self, rhs: Rational) -> Rational {
+        Rational {
+            top: self.top * rhs.bottom,
+            bottom: self.bottom * rhs.top,
+        }
+    }
+}
+
+impl cmp::PartialEq for Rational {
+    fn eq(&self, rhs: &Rational) -> bool {
+        self.top * rhs.bottom == self.bottom * rhs.top
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rational_test() {
+        assert!(Rational::new(1, 2) + Rational::new(1, 2) == Rational::from(1));
+        assert!(Rational::new(1, 2) - Rational::new(1, 2) == Rational::from(0));
+        assert!(Rational::new(1, 2) * Rational::new(1, 2) == Rational::new(1, 4));
+        assert!(Rational::new(1, 2) / Rational::from(2) == Rational::new(1, 4));
     }
 }
