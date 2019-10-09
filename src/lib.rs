a4adbd6b00c6ca79902fac5ae2ab778995650b2d
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-09 10:42:44 -0700

    Moved a one used variable into its use location

--- src/lib.rs
+++ src/lib.rs
@@ -44,8 +44,7 @@ pub fn coprimes(val: u64) -> Vec<u64> {
     use factoring::prime_factors;
     let input_factors = prime_factors(val);
 
-    let possibles = 2..val;
-    let guesses = possibles.map(|val| (val, prime_factors(val)));
+    let guesses = (2..val).map(|val| (val, prime_factors(val)));
 
     let correct_guesses = guesses.filter(|(_, factors)| {
         for factor in &input_factors {
